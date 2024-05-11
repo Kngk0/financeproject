0cf6ee24804fc7d709fbe58c720934d41f336140
Kngk0 <k_westbrook@u.pacific.edu>
2024-05-10 17:35:39 -0700

    Added user expense input prompt

--- expensetracking.py
+++ expensetracking.py
@@ -1,6 +1,10 @@
 # Inputting, categorizing, saving, and controlling expenses.
 
 # 1. Users should be able to input their daily expenses, including the amount and category (e.g., groceries, utilities, entertainment).
+def expensetracking():
+	date = input('Please enter the date of the expense (YYYY-MM-DD): ')
+	amount = input('Please enter the amount of the expense: ')
+	category = input('Please enter a brief description or category for the expense: ')
 
 # 2. The application should allow users to add, remove, and update expenses.
 
