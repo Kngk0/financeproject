43c12b847d07c09a685366e6eadea82280750a69
Kngk0 <k_westbrook@u.pacific.edu>
2024-05-17 00:56:28 -0700

    Edited attributes

--- expensetracking.py
+++ expensetracking.py
@@ -2,11 +2,11 @@
 
 # 1. Users should be able to input their daily expenses, including the amount and category (e.g., groceries, utilities, entertainment).
 class expensetracking: 
- 	def __init__(self, user):
+ 	def __init__(self):
  		self.expense_item = []
- 		self.user = user
  		self.tracking_info = 0.0
 
+ 	def add_expense()
 # 2. The application should allow users to add, remove, and update expenses.
 
 # 3. Users should be able to view a summary of their expenses, categorized by expense type, and the total spending.
