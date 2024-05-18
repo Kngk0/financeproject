1a721adbe3b4dd8a339509486bd3f1f61d8400a0
Kngk0 <k_westbrook@u.pacific.edu>
2024-05-18 01:46:37 -0700

    Enhanced menu interface with expensetracking module integration and improved expense tracking features.

--- expensetracking.py
+++ expensetracking.py
@@ -1,12 +1,18 @@
 # Inputting, categorizing, saving, and controlling expenses.
 
 # 1. Users should be able to input their daily expenses, including the amount and category (e.g., groceries, utilities, entertainment).
-class expensetracking: 
- 	def __init__(self):
- 		self.expense_item = []
- 		self.tracking_info = 0.0
+class expensetracker: 
 
- 	def add_expense()
+ 	def __init__(self):
+ 		self.expenses = {
+ 							 'Date': [], 'Amount': [], 'Category': [], 
+ 							 'Description': [],'Payment Method': [], 'From': [],
+ 							 'Payment Status': [], 'Transaction ID': []
+ 							 }
+
+ 	def interface(self):
+
+
 # 2. The application should allow users to add, remove, and update expenses.
 
 # 3. Users should be able to view a summary of their expenses, categorized by expense type, and the total spending.
--- finance.py
+++ finance.py
@@ -1,5 +1,5 @@
 # user-friendly menu interface
-
+import expensetracking
 # 1. The application should feature a command-line interface that is intuitive and easy to navigate.
 def main_menu():
 	while True:
@@ -11,7 +11,14 @@ def main_menu():
 		choice = input('Enter your choice: ')
 
 		if choice == '1':
-			print('You selected Track expenses')
+			#print('You selected Track expenses')
+			track = expensetracking.expensetracker()
+			# expensetracker.interface()
+			# while expensetracker.interace is running, prompt user to add, remove, or update expense
+				# if user enters add, they will be prompted to add expense information and the expense data will be updated and then displayed on the interface
+				# if user enters remove, they will be prompted to remove expense information and the expense data will be updated and then displayed on the interface
+				# if user enters update, they will be prompted to update expense and the expense data will be updated and then displayed on the interface
+				# if user enters back, they will return to main menu
 		elif choice == '2':
 			print('You selected Manage Budget')
 		elif choice == '3':
