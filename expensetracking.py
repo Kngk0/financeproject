1a4907aac4f93d740a922fcf86ee896b244c1ee2
Kngk0 <k_westbrook@u.pacific.edu>
2024-05-19 22:30:51 -0700

    Moved and added detail to expense tracker interface in the main file

--- expensetracking.py
+++ expensetracking.py
@@ -6,42 +6,11 @@ class expensetracker:
  	def __init__(self):
  		self.expenses = {
  							 'Date': [], 'Amount': [], 'Category': [], 
- 							 'Description': [],'Payment Method': [], 'From': [],
+ 							 'Description': [],'Payment Method': [], 'To': [],
  							 'Payment Status': [], 'Transaction ID': []
  							 }
-
- 	def interface(self):
- 		while True:
- 			print('==============================')
- 			print('Expense Tracker Menu')
- 			print('==============================')
- 			print('1. Add Expense')
- 			print('2. View Expenses')
- 			print('3. Edit Expense')
- 			print('4. Delete Expense')
- 			print('5. Search Expense')
- 			print('6. Generate Report')
- 			print('7. Back')
- 			print('\n')
- 			choice = input('Enter your choice (1-7): ')
-
- 			if choice == '1':
- 				print('You selected Add expense')
- 			elif choice == '2':
- 				print('You selected View expenses')
- 			elif choice == '3':
- 				print('You selected Edit expense')
- 			elif choice == '4':
- 				print('You selected Delete expense')
- 			elif choice == '5':
- 				print('You selected Search expense')
- 			elif choice == '6':
- 				print('You selected Generate Report')
- 			elif choice == '7':
- 				print('You selected back')
- 				break
-
-
 # 2. The application should allow users to add, remove, and update expenses.
+ 	def add(self, expenses):
+
 
 # 3. Users should be able to view a summary of their expenses, categorized by expense type, and the total spending.
--- finance.py
+++ finance.py
@@ -9,25 +9,56 @@ def main_menu():
 		print('4. Exit')
 
 		choice = input('Enter your choice: ')
+		if choice.strip():
+			if choice == '1':
+				#print('You selected Track expenses')
+				track = expensetracking.expensetracker()
+				while True:
+	 				print('==============================')
+	 				print('Expense Tracker Menu')
+	 				print('==============================')
+	 				print('1. Add Expense')
+	 				print('2. View Expenses')
+	 				print('3. Edit Expense')
+	 				print('4. Delete Expense')
+	 				print('5. Search Expense')
+	 				print('6. Generate Report')
+	 				print('7. Back')
+	 				print('\n')
+	 				choice = input('Enter your choice (1-7): ')
 
-		if choice == '1':
-			#print('You selected Track expenses')
-			track = expensetracking.expensetracker()
-			track.interface()
-			# while expensetracker.interace is running, prompt user to add, remove, or update expense
-				# if user enters add, they will be prompted to add expense information and the expense data will be updated and then displayed on the interface
-				# if user enters remove, they will be prompted to remove expense information and the expense data will be updated and then displayed on the interface
-				# if user enters update, they will be prompted to update expense and the expense data will be updated and then displayed on the interface
-				# if user enters back, they will return to main menu
-		elif choice == '2':
-			print('You selected Manage Budget')
-		elif choice == '3':
-			print('You selected Calculator')
-		elif choice == '4':
-			print('Exiting program. Goodbye!')
-			break
+	 				if choice == '1':
+	 					print('Please enter details of a new expense.')
+	 					date = input('Enter the date of the expense (YYYY-MM-DD): ')
+	 					amount = input('Enter the amount spent: ')
+	 					category = input('Enter the category for this expense: ')
+	 					description = input('Optionally, add a brief description for this expense:\n')
+	 					payment = input('Specify the payment method: ')
+	 					to = input('Enter the recipient: ')
+	 					status = input('Paid or Pending: ')
+	 				elif choice == '2':
+	 					print('You selected View expenses')
+	 				elif choice == '3':
+	 					print('You selected Edit expense')
+	 				elif choice == '4':
+	 					print('You selected Delete expense')
+	 				elif choice == '5':
+	 					print('You selected Search expense')
+	 				elif choice == '6':
+	 					print('You selected Generate Report')
+	 				elif choice == '7':
+	 					print('You selected back')
+	 					break
+			elif choice == '2':
+				print('You selected Manage Budget')
+			elif choice == '3':
+				print('You selected Calculator')
+			elif choice == '4':
+				print('Exiting program. Goodbye!')
+				break
 		else:
 			print('Invalid choice. Please select a number from the menu')
+
 
 main_menu()
 
