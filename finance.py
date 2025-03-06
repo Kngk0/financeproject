8811391b616057b02b4295671cf3f6f922ac0342
Kngk0 <k_westbrook@u.pacific.edu>
2025-03-05 23:29:40 -0800

    fixed indentations

--- finance.py
+++ finance.py
@@ -1,13 +1,13 @@
 # user-friendly menu interface
 import transactionTracking
-import budgetmanagement
+import budgetManagement
 import datetime
 import calculator
 # 1. The application should feature a command-line interface that is intuitive and easy to navigate.
 # 2. Users should be able to interact with the application through text-based commands and receive informative prompts and messages
 def mainMenu():
 	track = transactionTracking.transactionTracker()
-	manage = budgetmanagement.management()
+	manage = budgetManagement.management()
 	calculate = calculator.calculator()
 	while True:
 		print('==============================')
@@ -22,147 +22,147 @@ def mainMenu():
 		if choice.strip():
 			if choice == '1':
 				while True:
-	 				print('==============================')
-	 				print('Transaction Tracker Menu')
-	 				print('==============================')
-	 				print('1. Add Expense')
-	 				print('2. Add Income')
-	 				print('3. View Transactions')
-	 				print('4. Search Transactions')
-	 				print('5. Back\n')
-	 				choice = input('Enter your choice (1-5): ')
-	 				print()
-	 				if choice == '1':
-	 					print('Please enter details of a new expense.')
-	 					while True:
-	 						date = input('Enter the date of the expense (YYYY-MM-DD): ').strip()
-	 						if not date:
-	 							print('Please enter the date for this expense.')
-	 							continue
-	 						try:
-	 							date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
-	 							if date <= datetime.datetime.today().date():
-	 								break
-	 							else:
-	 								print('The date cannot be in the future. Please enter a valid date.')
-	 						except ValueError:
-	 							print('Invalid date format. Please enter the date in the format YYYY-MM-DD.')
-	 					if date > datetime.datetime.today().date():
-	 						payment_status = 'pending'
-	 					else:
-	 						payment_status = 'paid'
-	 					while True:
-	 						amount = input('Enter the amount spent: $').strip()
-	 						if not amount:
-	 							print('Please enter the amount for this expense.')
-	 							continue
-	 						try:
-	 							amount = float(amount.replace(',',''))
-	 							if amount <= 0:
-	 								print('Amount must be a positive number.')
-	 							else:
-	 								break
-	 						except ValueError:
-	 							print('Invalid amount. Please enter a valid number.')
-	 					while True:
-	 						category = input('Enter the category for this expense: ').strip()
-	 						if not category:
-	 							print('Please enter the category for this expense.')
-	 						else:
-	 							break
-	 					description = input('Optionally, add a brief description for this expense:\n').strip()
-	 					if description:
-	 						pass
-	 					else:
-	 						print('No description provided for this expense.')
-	 					method = ['cash', 'credit card', 'debit card', 'bank transfer', 'check']
-	 					while True:
-	 						payment = input('Specify the payment method (Cash, Credit Card, Debit Card, Bank Transfer, Check): ').strip().lower()
-	 						if payment in method:
-	 							break
-	 						else:
-	 							print('Please enter a valid payment method from the options provided.')
-	 					while True:
-	 						to = input('Enter the recipient: ').strip()
-	 						if not to:
-	 							print('Please enter the recipient of this expense.')
-	 						else:
-	 							break
-	 					print('\nExpense added successfully:')
-	 					track.addExpense(date, amount, category, payment, to, payment_status, description)
-	 					print()
-	 				elif choice == '2':
-	 					print('Please enter details of a new income.')
-	 					while True:
-	 						date = input('Enter the date of the income (YYYY-MM-DD): ').strip()
-	 						if not date:
-	 							print('Please enter the date for this income.')
-	 							continue
-	 						try:
-	 							date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
-	 							break
-	 						except ValueError:
-	 							print('Invalid date format. Please enter the date in the format YYYY-MM-DD.')
-	 					if date > datetime.datetime.today().date():
-	 						status = 'pending'
-	 					else:
-	 						status = 'paid'
-	 					while True:
-	 						amount = input('Enter the amount of income: $').strip()
-	 						if not amount:
-	 							print('Please enter the amount for this expense.')
-	 							continue
-	 						try:
-	 							amount = float(amount.replace(',',''))
-	 							if amount <= 0:
-	 								print('Amount must be a positive number.')
-	 							else:
-	 								break
-	 						except ValueError:
-	 							print('Invalid amount. Please enter a valid number.')
-	 					while True:
-	 						category = input('Enter the category for this income: ').strip()
-	 						if not category:
-	 							print('Please enter the category for this income.')
-	 						else:
-	 							break
-	 					description = input('Optionally, add a brief description for this income:\n').strip()
-	 					if description:
-	 						pass
-	 					else:
-	 						print('No description provided for this income.')
-	 					incomeType = ['cash', 'credit', 'bank transfer', 'check']
-	 					while True:
-	 						type = input('Specify the income method (Cash, Credit, Bank Transfer, Check): ').strip().lower()
-	 						if type in incomeType:
-	 							break
-	 						else:
-	 							print('Please enter a valid income method from the options provided.')
-	 					while True:
-	 						source = input('Enter the source: ').strip()
-	 						if not source:
-	 							print('Please enter the source of this income.')
-	 						else:
-	 							break
-	 					print('\nIncome added successfully:')
-	 					track.addIncome(date, amount, category, type, source, status, description)
-	 					print()
-	 				elif choice == '3':
-	 					track.view()
-	 					# Check if both expenses and incomes are empty
-	 					if not track.expenses and not track.incomes:
-	 						print('No transactions to show.\n')
-	 				elif choice == '4':
-	 					while True:
-	 						search = input('Search (or type "exit" to quit): ')
-	 						if search.lower() == 'exit':
-	 							break
-	 						track.search(search)
-	 						print()
-	 				elif choice == '5':
-	 					break
-	 				else:
-	 					print('Please enter a valid choice (1-4)\n')
+					print('==============================')
+					print('Transaction Tracker Menu')
+					print('==============================')
+					print('1. Add Expense')
+					print('2. Add Income')
+					print('3. View Transactions')
+					print('4. Search Transactions')
+					print('5. Back\n')
+					choice = input('Enter your choice (1-5): ')
+					print()
+					if choice == '1':
+						print('Please enter details of a new expense.')
+						while True:
+							date = input('Enter the date of the expense (YYYY-MM-DD): ').strip()
+							if not date:
+								print('Please enter the date for this expense.')
+								continue
+							try:
+								date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+								if date <= datetime.datetime.today().date():
+									break
+								else:
+									print('The date cannot be in the future. Please enter a valid date.')
+							except ValueError:
+								print('Invalid date format. Please enter the date in the format YYYY-MM-DD.')
+						if date > datetime.datetime.today().date():
+							payment_status = 'pending'
+						else:
+							payment_status = 'paid'
+						while True:
+							amount = input('Enter the amount spent: $').strip()
+							if not amount:
+								print('Please enter the amount for this expense.')
+								continue
+							try:
+								amount = float(amount.replace(',',''))
+								if amount <= 0:
+									print('Amount must be a positive number.')
+								else:
+									break
+							except ValueError:
+								print('Invalid amount. Please enter a valid number.')
+						while True:
+							category = input('Enter the category for this expense: ').strip()
+							if not category:
+								print('Please enter the category for this expense.')
+							else:
+								break
+						description = input('Optionally, add a brief description for this expense:\n').strip()
+						if description:
+							pass
+						else:
+							print('No description provided for this expense.')
+						method = ['cash', 'credit card', 'debit card', 'bank transfer', 'check']
+						while True:
+							payment = input('Specify the payment method (Cash, Credit Card, Debit Card, Bank Transfer, Check): ').strip().lower()
+							if payment in method:
+								break
+							else:
+								print('Please enter a valid payment method from the options provided.')
+						while True:
+							to = input('Enter the recipient: ').strip()
+							if not to:
+								print('Please enter the recipient of this expense.')
+							else:
+								break
+						print('\nExpense added successfully:')
+						track.addExpense(date, amount, category, payment, to, payment_status, description)
+						print()
+					elif choice == '2':
+						print('Please enter details of a new income.')
+						while True:
+							date = input('Enter the date of the income (YYYY-MM-DD): ').strip()
+							if not date:
+								print('Please enter the date for this income.')
+								continue
+							try:
+								date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+								break
+							except ValueError:
+								print('Invalid date format. Please enter the date in the format YYYY-MM-DD.')
+						if date > datetime.datetime.today().date():
+							status = 'pending'
+						else:
+							status = 'paid'
+						while True:
+							amount = input('Enter the amount of income: $').strip()
+							if not amount:
+								print('Please enter the amount for this expense.')
+								continue
+							try:
+								amount = float(amount.replace(',',''))
+								if amount <= 0:
+									print('Amount must be a positive number.')
+								else:
+									break
+							except ValueError:
+								print('Invalid amount. Please enter a valid number.')
+						while True:
+							category = input('Enter the category for this income: ').strip()
+							if not category:
+								print('Please enter the category for this income.')
+							else:
+								break
+						description = input('Optionally, add a brief description for this income:\n').strip()
+						if description:
+							pass
+						else:
+							print('No description provided for this income.')
+						incomeType = ['cash', 'credit', 'bank transfer', 'check']
+						while True:
+							type = input('Specify the income method (Cash, Credit, Bank Transfer, Check): ').strip().lower()
+							if type in incomeType:
+								break
+							else:
+								print('Please enter a valid income method from the options provided.')
+						while True:
+							source = input('Enter the source: ').strip()
+							if not source:
+								print('Please enter the source of this income.')
+							else:
+								break
+						print('\nIncome added successfully:')
+						track.addIncome(date, amount, category, type, source, status, description)
+						print()
+					elif choice == '3':
+						track.view()
+						# Check if both expenses and incomes are empty
+						if not track.expenses and not track.incomes:
+							print('No transactions to show.\n')
+					elif choice == '4':
+						while True:
+							search = input('Search (or type "exit" to quit): ')
+							if search.lower() == 'exit':
+								break
+							track.search(search)
+							print()
+					elif choice == '5':
+						break
+					else:
+						print('Please enter a valid choice (1-4)\n')
 			elif choice == '2':
 				while True:
 					print('==============================')
--- transactionTracking.py
+++ transactionTracking.py
@@ -1,12 +1,12 @@
 class transactionTracker: 
 
- 	def __init__(self):
- 		self.expenses = []
- 		self.incomes = []
+	def __init__(self):
+		self.expenses = []
+		self.incomes = []
 
 	# Method to add expenses
- 	def addExpense(self, date, amount, category, payment, to, status,  description = ''):
- 		expense = {
+	def addExpense(self, date, amount, category, payment, to, status,  description = ''):
+		expense = {
  			"Date": date,
  			"Amount": amount,
  			"Category": category,
@@ -14,18 +14,19 @@ class transactionTracker:
  			"Payment Method": payment,
  			"To": to,
  			"Payment Status": status
- 		}
- 		self.expenses.append(expense)
- 		for key, value in expense.items():
- 			if key == 'Amount':
- 				print(f'{key}: ${value:,.2f}')
- 			else:
- 				print(f'{key}: {value}')
-
- 	# Method to add income
- 	def addIncome(self, date, amount, category, incomeType, source, status, description = ''):
+		}
+		
+		self.expenses.append(expense)
+		for key, value in expense.items():
+			if key == 'Amount':
+				print(f'{key}: ${value:,.2f}')
+			else:
+				print(f'{key}: {value}')
+				 
+	# Method to add income
+	def addIncome(self, date, amount, category, incomeType, source, status, description = ''):
  		# DEFINE income AS DICTIONARY OF DATE, AMOUNT, CATEGORY, DESCRIPTION, INCOME TYPE, SOURCE, STATUS
- 		income = {
+		income = {
  			"Date": date,
  			"Amount": amount,
  			"Category": category,
@@ -34,86 +35,86 @@ class transactionTracker:
  			"Source": source,
  			"Status": status
  		}
+		 
+		self.incomes.append(income)
+		for key, value in income.items():
+			if key == "Amount":
+				print(f'{key}: ${value:,.2f}')
+			else:
+				print(f'{key}: {value}')
 
- 		self.incomes.append(income)
- 		for key, value in income.items():
- 			if key == "Amount":
- 				print(f'{key}: ${value:,.2f}')
- 			else:
- 				print(f'{key}: {value}')
-
- 	# Method to view expenses
- 	def view(self):
- 		summary = {
+# Method to view expenses
+	def view(self):
+		summary = {
  			"Total Expenses": 0,
  			"Total Income": 0
  		}
+		
+		totalExpenses = 0
+		
+		for expense in self.expenses:
+			print(f'Date: {expense['Date']}')
+			print(f'Amount: {expense['Amount']:,.2f}')
+			print(f'Category: {expense['Category']}')
+			print(f'Description: {expense['Description']}')
+			print(f'Payment Mehod: {expense['Payment Method']}')
+			print(f'Merchent: {expense['To']}')
+			print(f'Payment Status: {expense['Payment Status']}')
+			print()
+			totalExpenses += expense["Amount"]
+			
+		summary["Total Expenses"] = f"${totalExpenses:,.2f}"
+		
+		# Initalize total income to zero
+		totalIncome = 0
+		
+		# Print income entries and calculate total income
+		for income in self.incomes:
+			print(f'Date: {income['Date']}')
+			print(f'Amount: {income['Amount']:,.2f}')
+			print(f'Category: {income['Category']}')
+			print(f'Description: {income['Description']}')
+			print(f'Income Type: {income['Income Type']}')
+			print(f'Source: {income['Source']}')
+			print(f'Status: {income['Status']}')
+			print()
+			totalIncome += income["Amount"]
 
- 		totalExpenses = 0
+		summary['Total Income'] = f"${totalIncome:,.2f}"
+		
+		print(summary)
 
- 		for expense in self.expenses:
- 			print(f'Date: {expense['Date']}')
- 			print(f'Amount: {expense['Amount']:,.2f}')
- 			print(f'Category: {expense['Category']}')
- 			print(f'Description: {expense['Description']}')
- 			print(f'Payment Mehod: {expense['Payment Method']}')
- 			print(f'Merchent: {expense['To']}')
- 			print(f'Payment Status: {expense['Payment Status']}')
- 			print()
- 			totalExpenses += expense["Amount"]
+	def search(self, searchTerm):
+		found = False
+		for expense in self.expenses:
+			for value in expense.values():
+				if searchTerm in str(value):
+					print(f'Date: {expense["Date"]}')
+					print(f'Amount: {expense["Amount"]:,.2f}')
+					print(f'Category: {expense["Category"]}')
+					print(f'Description: {expense["Description"]}')
+					print(f'Payment Method: {expense["Payment Method"]}')
+					print(f'To: {expense["To"]}')
+					print(f'Payment Status: {expense["Payment Status"]}')
+					print()
+					found = True
+					break
+				
+		for income in self.incomes:
+			for value in income.values():
+				if searchTerm in str(value):
+					print(f'Date: {income["Date"]}')
+					print(f'Amount: {income["Amount"]:,.2f}')
+					print(f'Category: {income["Category"]}')
+					print(f'Description: {income["Description"]}')
+					print(f'Income Type: {income["Income Type"]}')
+					print(f'Source: {income["Source"]}')
+					print(f'Status: {income["Status"]}')
+					print()
+					found = True
+					break
 
- 		summary["Total Expenses"] = f"${totalExpenses:,.2f}"
-
- 		# Initalize total income to zero
- 		totalIncome = 0
-
- 		# Print income entries and calculate total income
- 		for income in self.incomes:
- 			print(f'Date: {income['Date']}')
- 			print(f'Amount: {income['Amount']:,.2f}')
- 			print(f'Category: {income['Category']}')
- 			print(f'Description: {income['Description']}')
- 			print(f'Income Type: {income['Income Type']}')
- 			print(f'Source: {income['Source']}')
- 			print(f'Status: {income['Status']}')
- 			print()
- 			totalIncome += income["Amount"]
-
- 		summary['Total Income'] = f"${totalIncome:,.2f}"
-
- 		print(summary)
-
- 	def search(self, searchTerm):
- 		found = False
- 		for expense in self.expenses:
- 			for value in expense.values():
- 				if searchTerm in str(value):
- 					print(f'Date: {expense["Date"]}')
- 					print(f'Amount: {expense["Amount"]:,.2f}')
- 					print(f'Category: {expense["Category"]}')
- 					print(f'Description: {expense["Description"]}')
- 					print(f'Payment Method: {expense["Payment Method"]}')
- 					print(f'To: {expense["To"]}')
- 					print(f'Payment Status: {expense["Payment Status"]}')
- 					print()
- 					found = True
- 					break
-
- 		for income in self.incomes:
- 			for value in income.values():
- 				if searchTerm in str(value):
- 					print(f'Date: {income["Date"]}')
- 					print(f'Amount: {income["Amount"]:,.2f}')
- 					print(f'Category: {income["Category"]}')
- 					print(f'Description: {income["Description"]}')
- 					print(f'Income Type: {income["Income Type"]}')
- 					print(f'Source: {income["Source"]}')
- 					print(f'Status: {income["Status"]}')
- 					print()
- 					found = True
- 					break
-
- 		if not found:
- 			print('No results for "' + searchTerm + '"')
- 			print('Check the spelling or try a new search.')
- 			print()
+		if not found:
+			print('No results for "' + searchTerm + '"')
+			print('Check the spelling or try a new search.')
+			print()
